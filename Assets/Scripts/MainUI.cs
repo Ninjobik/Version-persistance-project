cf042b9a566e1efaa05b2ce1db439dd2559d1c2b
Ninjobik <32847103+Ninjobik@users.noreply.github.com>
2021-10-04 07:46:21 +0300

    Project Done
    added data persistence between scenes
    added data persistence between sessions

---
+++ Assets/Scripts/MainUI.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainUI : MonoBehaviour
+{
+    public string bestScoreName;
+    public string bestScore;
+    public TMP_InputField nameField;
+    public TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        ScoreManager.instance.LoadData();
+
+        if (ScoreManager.instance.activePlayerName.Length > 0)
+        {
+            nameField.text = ScoreManager.instance.activePlayerName;
+        }
+
+        if (ScoreManager.instance.heighScore > 0)
+        { 
+            scoreText.text = $"{ScoreManager.instance.heighScoreName} : {ScoreManager.instance.heighScore}";
+        }
+        else
+        {
+            scoreText.text = "No High Score Yet";
+        }
+    }
+
+    public void ApplyName()
+    {
+        ScoreManager.instance.activePlayerName = nameField.text;
+    }
+
+    public void StartGame()
+    {
+        ApplyName();
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        ScoreManager.instance.SaveDate();
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+}
